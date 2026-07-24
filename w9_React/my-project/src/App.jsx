cc7ba97fae3884ae59589b25365b0fb53588c3aa
Mahendra <mahendrapowar1365@gmail.com>
2026-07-24 21:42:51 +0530

    basic counter app

--- w9_React/my-project/src/App.jsx
+++ w9_React/my-project/src/App.jsx
@@ -1,9 +1,34 @@
 import { useState } from 'react'
 
-import './App.css'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <div> <h1>hi there</h1>
+  <Counter></Counter>
+ 
+</div>
+}
+
+function Counter(){
+   const [count, setCount] = useState(0);
+
+   function Increse(){
+    setCount(count + 1)
+   }
+   function Decrese(){
+    setCount(count - 1)
+   }
+   function Reset(){
+    setCount(0)
+   }
+
+
+   return <div>
+    <h1>{count}</h1>
+    <button onClick={Increse}>Increase</button>
+    <button onClick={Decrese}>Decrease count</button>
+    <button onClick={Reset}>Reset count</button>
+   </div>
 
 }
 
