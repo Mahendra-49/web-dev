9fb5fe388956b645e85d706b48b2561962a80d29
Mahendra <mahendrapowar1365@gmail.com>
2026-07-29 10:24:12 +0530

    postcomponent

--- w9_React/my-project/src/App.jsx
+++ w9_React/my-project/src/App.jsx
@@ -1,66 +1,49 @@
 import { useState ,useEffect} from 'react'
 
-// import './App.css'
-
-// learning - dependency array, useEffect, cleanup
 
 function App() {
  
-  // const [countervisible , setCountervisible] = useState(true)
-    const [count , setCount] = useState(0)
+  
+  return (
+     
+   <div style={{ backgroundColor: "lightblue",height:"100vh" }}>
+   
+    <div style={{display:"flex", justifyContent:"center"}}>
+      <div >
+        <div style={{padding:10}}><PostComponent/></div>
+        <div style={{padding:10}}><PostComponent/></div>
+        <div style={{padding:10}}><PostComponent/></div>
+      </div>
+    </div>
+    
+   </div>
+  )
+}
 
-  // useEffect(function(){
-  //   setInterval(function (){
-  //     setCountervisible(c =>!c)
-  //   },5000)
-  // },[])
+ //object 
+const style = { width:  350, backgroundColor:"white", borderRadius:10, borderWidth:1, borderColor:"gray", padding:20}
+ 
+  function PostComponent(){
+    return (<div style={style}>
+    <div style={{display:"flex"}}>
+      <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyh9ZR7j2Oi5JHGSIe2mt2cgeVlwQb4mXg3kXIaPgEJQ&s=10"} style={{height:70, width :70, borderRadius:36 }}/>
+    
+    <div style={{marginLeft:10, width:200, marginTop:10}}>
+      <div>
+      <b>username</b>
+    </div>
+    <div>23,445 followers</div>
+    <div>12 min</div>
+    </div>
+    
+    </div>
 
-  function increse(){
-    setCount(c => c+ 1)
-
+    <div> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione veniam ad libero cumque cum totam voluptas natus, enim ipsa est veritatis inventore ut quasi nobis tenetur nam, nisi repellat molestiae?</div>
+    </div>)
   }
 
-  return <div>
-    {<Counter count={count} />}
-    <button onClick={increse}>Increase count</button>
-  </div>
- 
   
-}
 
-function Counter(props){
- 
-
-   useEffect(function (){
-    const clk=setInterval(function (){
-      setCount(c => c+1)
-    },1000)
-
-    return function(){
-      clearInterval(clk)
-    }
-
-   },[])
-
-   //dependency array with value:-when the count change then only the function execute 
-   useEffect(function(){
-    console.log("count has changed")
-   },[props.count])
-
-
-
-   function reset(){
-    return setCount(0)
-   }
-
-
-
-  return <div>
-
-    <h1>{props.count}</h1>
-    <button onClick={reset}>Reset count</button>
-  </div>
-}
 
 
 
