f26a26d217efccb5235377b681e3c91b7456aefa
Mahendra <mahendrapowar1365@gmail.com>
2026-07-30 23:13:22 +0530

    Post file,addpost added

--- w9_React/my-project/src/App.jsx
+++ w9_React/my-project/src/App.jsx
@@ -1,32 +1,44 @@
+ import { useState ,useEffect} from 'react'
+ import { PostComponent } from './Post'
 
-import{useState} from 'react'
 
-function App(){
+function App() {
+ const[posts, setPosts]= useState([])
 
-  return (
-    <div>
-       <div> <Togglefunction/></div>
-       <div> <Togglefunction/></div>
-       <div> <Togglefunction/></div>
 
-    </div>
+  const postComponents = posts.map(post =><PostComponent
+    name ={post.name}
+    subtitle={ post.subtitle}
+    time ={post.time}
+    image={ post.image}
+    description ={ post.description}
+    />
   )
-}
 
-function Togglefunction(){
+  function addPost(){
+    setPosts([...posts,{
+       name:"harkirat",
+    subtitle:"22000 followers",
+    time:"2m ago",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyh9ZR7j2Oi5JHGSIe2mt2cgeVlwQb4mXg3kXIaPgEJQ&s=10",
+    
+    description:"what to know to win that match? Checkout it now!"
 
-  const[notificationCount, setnotificationCount]= useState(0)
+    }])
 
-  console.log("re-render");
-  function mess(){
-    setnotificationCount(notificationCount + 1)
   }
-  return(
-    <div>
-      <button onClick={mess}>Toggle message</button>
-      {notificationCount}
+  
+  return (
+     
+   <div style={{ backgroundColor: "#dfe6e9",height:"100vh" }}>
+   <button onClick={addPost}>Add post</button>
+   <div style={{display:"flex", justifyContent:"center"}}>
+    <div> 
+      {postComponents}
     </div>
+   </div>
+
+    
+   </div>
   )
 }
-
-export default App;
