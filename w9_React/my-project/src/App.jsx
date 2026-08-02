2557813c426bc663b86e62dbd33a88a469e00dc2
Mahendra <mahendrapowar1365@gmail.com>
2026-08-02 23:21:46 +0530

    children generic card

--- w9_React/my-project/src/App.jsx
+++ w9_React/my-project/src/App.jsx
@@ -118,5 +118,46 @@ import {useState , useEffect} from 'react';
 
  
 
+//9.4 offline
 
-export default App
+import { useState, useEffect } from "react";
+
+function App(){
+
+  return (
+        <div>
+            <Card>
+                <h2>Card Title</h2>
+                <p>This is some content inside the card.</p>
+            </Card>
+            <Card>
+                <h2>Another Card</h2>
+                <textarea placeholder="Write post"/>
+                <p>This card has different content!</p>
+            </Card>
+        </div>
+    )
+}
+
+function Card({children}){
+
+return (
+  <div>
+ <div style={{
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            padding: '20px',
+            margin: '10px',
+            boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.1)',
+        }}>     {children}
+  </div>
+   </div>
+)
+
+}
+ 
+
+ export default App;
+
+
+
