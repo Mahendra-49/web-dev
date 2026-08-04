9e7b4ca75a5ee8bbf453a8315f7e27525e723093
Mahendra <mahendrapowar1365@gmail.com>
2026-08-04 23:38:45 +0530

    lists and keys

--- w9_React/my-project/src/App.jsx
+++ w9_React/my-project/src/App.jsx
@@ -160,4 +160,29 @@ return (
  export default App;
 
 
+//9.4 offline lists and keys
 
+
+function App(){
+
+ return(
+  <div>
+   {[
+     <Todo key={1} title={"Go to gym"} done={false}/>,
+     <Todo key={2} title={"Eat food"} done={true}/>
+   ]}
+  </div>
+ );
+}
+
+
+function Todo(title,done){
+
+return(
+ <div>
+    {title} - {done ? "Done" : "Not Done" }
+ </div>
+)
+}
+
+export deafult App;
